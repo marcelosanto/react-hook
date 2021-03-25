21df90d56651e06d37324eb7586790e1608667e5
Marcelo Santos <rocha.mer21@gmail.com>
2021-03-25 17:09:26 -0300

    feat: usestate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  return (
-    <div className='App'>
-      <p>oiee</p>
-    </div>
-  )
+    const [counter, setCounter] = useState(0)
+
+    const handlerIncrement = () => {
+        setCounter((counter) => counter + 1)
+    }
+
+    return (
+        <div className="App">
+            <h1>Contador: {counter}</h1>
+            <p>
+                <button type="button" onClick={handlerIncrement}>
+                    Increment
+                </button>
+            </p>
+        </div>
+    )
 }
 
 export default App
