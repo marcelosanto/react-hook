801d0914826a5220041dbaf137c66c796c20bcf0
Marcelo Santos <rocha.mer21@gmail.com>
2021-04-04 16:37:52 -0300

    reorganizando o projeto

--- src/contexts/AppContext/data.js
+++
@@ -1,5 +0,0 @@
-export const globalState = {
-    title: 'O Titulo que contexto',
-    body: 'O body do contexto',
-    counter: 0,
-}
--- src/reducers/reducer.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-export const reducer = (state, action) => {
-    switch (action.type) {
-        case 'CHANGE_TITLE':
-            console.log('Mudar o titulo')
-            return { ...state, title: 'Deu certo man' }
-    }
-    return { ...state }
-}
